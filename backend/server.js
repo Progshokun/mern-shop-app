743590ae18a82d02aac3910f1b2e323c50d0a147
Oleg Lapin <pankiller11@gmail.com>
2026-03-10 15:21:16 +0300

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import router from "./routes/product.route.js";
+import cors from "cors";
 
 dotenv.config();
 
