13d1bb06bcba28ff519f188bf729115a95254c44
Oleg Lapin <pankiller11@gmail.com>
2026-03-10 13:00:03 +0300

    completel backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import router from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/products", router);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
